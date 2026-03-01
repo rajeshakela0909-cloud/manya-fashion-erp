f9991b09cd216a120105b2072982a815f0207172
rajeshakela0909-cloud <rajeshakela0909@gmail.com>
2026-03-01 14:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,9 @@ def sell():
 
     return redirect("/")
 
-app.run(host="0.0.0.0", port=5000)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
