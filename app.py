2a044647cc69e5c79a9c44c0c61b00585e4d0319
rajeshakela0909-cloud <rajeshakela0909@gmail.com>
2026-03-01 15:24:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS products (code TEXT PRIMARY KEY, name TEXT, purchase DOUBLE PRECISION,
 selling DOUBLE PRECISION, stock INTEGER)")
 cursor.execute("""
-cursor.execute("""
 CREATE TABLE IF NOT EXISTS products (
     code TEXT PRIMARY KEY,
     name TEXT,
@@ -125,6 +124,7 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
